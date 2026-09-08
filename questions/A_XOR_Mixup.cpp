766120f7e361a720cf30dc97462f3bfd4b3b9cea
ewerton <ewertonfernandeslira@gmail.com>
2026-09-08 15:25:06 -0300

    new question

---
+++ questions/A_XOR_Mixup.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) {
+        int n, a; cin >> n;
+        for(int i = 0; i < n; i++) cin >> a;
+        cout << a << '\n';
+    }
+}
